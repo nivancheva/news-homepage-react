832d327fe0c65425f809a11347305e7f4f665e91
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-04 22:30:45 +0300

    refactor to simplify code and remove repetition

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,60 @@ import imageWebMobile from './images/image-web-3-mobile.jpg'
 import logo from './images/logo.svg';
 import iconMenu from './images/icon-menu.svg';
 import iconMenuClose from './images/icon-menu-close.svg';
-import gamingImg from './images/image-gaming-growth.jpg';
-import laptopImg from './images/image-top-laptops.jpg';
-import pcsImg from './images/image-retro-pcs.jpg';
 import { useState } from 'react';
+
+const topics = [
+  {
+    title: "Reviving Retro PCs",
+    text: "What happens when old PCs are given modern upgrades?",
+    image: "image-retro-pcs.jpg"
+  },
+  {
+    title: "Top 10 Laptops of 2022",
+    text: "Our best picks for various needs and budgets.",
+    image: "image-top-laptops.jpg"
+  },
+  {
+    title: "The Growth of Gaming",
+    text: "How the pandemic has sparked fresh opportunities.",
+    image: "image-gaming-growth.jpg"
+  }
+];
+
+const menuLinks = [
+  "Home",
+  "New",
+  "Popular",
+  "Trending",
+  "Categories"
+];
+const news = [
+  {
+    title: "Hydrogen VS Electric Cars",
+    text: "Will hydrogen-fueled cars ever catch up to EVs?"
+  },
+  {
+    title: "The Downsides of AI Artistry",
+    text: "What are the possible adverse effects of on-demand AI image generation?"
+  },
+  {
+    title: "Is VC Funding Drying Up?",
+    text: "Private funding by VC firms is down 50% YOY. We take a look at what that means."
+  }
+];
 
 function App() {
   const [menuVisible, setMenuVisible] = useState(false);
 
-  function handleShowMenu() {
-    setMenuVisible(true);
+  function toggleMenu() {
+    setMenuVisible(!menuVisible);
   }
 
-  function handleHideMenu() {
-    setMenuVisible(false);
-  }
+  function pad(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
 
   return (
     <div className='wrapper'>
@@ -26,19 +65,17 @@ function App() {
         <div className='flex-container nav'>
           <img src={logo}/>
           <ul className={ `nav-bar ${menuVisible ? "" : "mobile-menu-hidden"} `}>
-            <li><a className='links'>Home</a></li>
-            <li><a className='links'>New</a></li>
-            <li><a className='links'>Popular</a></li>
-            <li><a className='links'>Trending</a></li>
-            <li><a className='links'>Categories</a></li>
+            {menuLinks.map((menuLink, idx) => {
+              return (
+                <li key={idx}><a className='links'>{menuLink}</a></li>
+              );
+            })}
           </ul>
           <div>
             {
-              menuVisible
-              ?
-              <button onClick={handleHideMenu} className={ `menuClose ${menuVisible ? "" : "mobile-menu-hidden"} `}><img src={iconMenuClose}/></button>
-              :
-              <button onClick={handleShowMenu} className='hamburger'><img src={iconMenu}/></button>
+              <button onClick={toggleMenu} className={menuVisible ? "menuClose" : "hamburger"}>
+                <img src={menuVisible ? iconMenuClose : iconMenu}/>
+              </button>
             }         
           </div>
         </div>
@@ -63,48 +100,30 @@ function App() {
         <div className='news-section'>
           <h2>New</h2>
           <div className='news-section-data'>
-            <div className='decoration'>
-             <a className='links links-new' href='#'><h3>Hydrogen VS Electric Cars</h3></a>
-              <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-            </div>
-            <div className='decoration'>
-              <a className=' links links-new' href='#'><h3>The Downsides of AI Artistry</h3></a>
-              <p>What are the possible adverse effects of on-demand AI image generation?</p>
-            </div>
-            <div>
-              <a className='links links-new'  href='#'><h3>Is VC Funding Drying Up?</h3></a>
-              <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
-            </div>
+            {news.map((newsItem, idx) => {
+              return (
+                <div key={idx} className='decoration'>
+                  <a className='links links-new' href='#'><h3>{newsItem.title}</h3></a>
+                  <p>{newsItem.text}</p>
+                </div>
+              )
+            })}
           </div>
         </div>
 
         <div className='container articles'>
-          <div className='flex-container articles-data'>
-            <img className='articles-img' src={pcsImg}/>
-            <div className='article'>
-              <p className='num'>01</p>
-              <a className='links' href='#'><h4>Reviving Retro PCs</h4></a>
-              <p className='article-description'>What happens when old PCs are given modern upgrades?</p>
-            </div>
-          </div>
-
-          <div className='flex-container articles-data'>
-            <img className='articles-img' src={laptopImg}/>
-            <div className='article'>
-              <p className='num'>02</p>
-              <a className='links' href='#'><h4>Top 10 Laptops of 2022</h4></a>
-              <p className='article-description'>Our best picks for various needs and budgets.</p>
-            </div>
-          </div>
-
-          <div className='flex-container articles-data'>
-            <img className='articles-img' src={gamingImg}/>
-            <div className='article'>
-              <p className='num'>03</p>
-              <a className='links' href='#'><h4>The Growth of Gaming</h4></a>
-              <p className='article-description'>How the pandemic has sparked fresh opportunities.</p>
-            </div>
-          </div>
+          {topics.map((topic, idx) => {
+            return (
+              <div key={idx} className='flex-container articles-data'>
+                <img className='articles-img' src={`./images/${topic.image}`}/>
+                <div className='article'>
+                  <p className='num'>{pad(idx + 1, 2)}</p>
+                  <a className='links' href='#'><h4>{topic.title}</h4></a>
+                  <p className='article-description'>{topic.text}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </main>    
     </div>
